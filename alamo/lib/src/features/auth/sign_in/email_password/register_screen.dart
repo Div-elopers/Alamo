7c35959eee578a7767d2321941377e6cd80ff3ee
yevitale <yesica.vitale@estudiantes.utec.edu.uy>
2024-10-20 11:16:13 -0300

    Update register screen
    All fields added

--- alamo/lib/src/features/auth/sign_in/email_password/register_screen.dart
+++ alamo/lib/src/features/auth/sign_in/email_password/register_screen.dart
@@ -11,7 +11,7 @@ class RegisterScreen extends StatelessWidget {
   final TextEditingController _stateController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _repitepassController = TextEditingController();
+  final TextEditingController _repitpassController = TextEditingController();
   // Clave para el formulario
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -20,165 +20,247 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25.0),
-          child: Text(
-            'Registro de usuario',
-            style: TextStyle(
-              color: Color(0xff1B1C41), // Color del texto
-              fontSize: 20, // Tamaño del texto
-              fontFamily: 'Sofia Sanz',
+        appBar: AppBar(
+          title: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              'Registro de usuario',
+              style: TextStyle(
+                color: Color(0xff1B1C41), // Color del texto
+                fontSize: 20, // Tamaño del texto
+                fontFamily: 'Sofia Sanz',
+              ),
             ),
           ),
-        ),
-        backgroundColor: Colors.transparent, // Establece el fondo transparente
-        elevation: 0,
-        iconTheme: const IconThemeData(color: Color(0xff1B1C41)),
-        leading: IconButton(
-          icon: const Icon(
-              Icons.arrow_back_ios), // Cambia esto por tu icono personalizado
-          color: const Color(0xff1B1C41),
-          iconSize: 20, // Tamaño del icono
-          onPressed: () {
-            Navigator.of(context).pop(); // Navega hacia atrás
-          },
-        ),
-      ),
-      body: ResponsiveScrollableCard(
-        child: FocusScope(
-          child: Form(
-            key: _formKey,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  const SizedBox(height: 20), // Espacio superior
-                  // Campo de nombre
-                  const Text('Nombre'),
-                  const SizedBox(height: 8), // Espacio entre texto y campo
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      // labelText: 'Nombre',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingresa tu nombre';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  // Campo de correo
-                  const Text('Correo Electrónico'),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      //labelText: 'Correo Electrónico',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingresa tu correo electrónico';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  // Campo de contraseña
-                  const Text('Contraseña'),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.transparent),
-                      ),
-                      //labelText: 'Contraseña',
-                    ),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingresa tu contraseña';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 32),
-                  /*ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _register(); // Lógica de registro
-                    }
-                  },
-                  child: const Text('Registrarse'),
-                ),*/
-                  PrimaryButton(
-                    text:
-                        'Registrarse', // Asegúrate de que este texto sea el adecuado
-                    backgroundColor: const Color(0xff1B1C41),
-                    isLoading:
-                        _isLoading, // Aquí puedes manejar el estado de carga
-                    onPressed: _isLoading
-                        ? null
-                        : () {
-                            if (_formKey.currentState!.validate()) {
-                              _register(); // Lógica de registro
-                            }
-                          },
-                  ),
-                ]),
+          backgroundColor:
+              Colors.transparent, // Establece el fondo transparente
+          elevation: 0,
+          iconTheme: const IconThemeData(color: Color(0xff1B1C41)),
+          leading: IconButton(
+            icon: const Icon(
+                Icons.arrow_back_ios), // Cambia esto por tu icono personalizado
+            color: const Color(0xff1B1C41),
+            iconSize: 20, // Tamaño del icono
+            onPressed: () {
+              Navigator.of(context).pop(); // Navega hacia atrás
+            },
           ),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: ResponsiveScrollableCard(
+            child: FocusScope(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      const SizedBox(height: 20), // Espacio superior
+                      // Campo de nombre
+                      const Text('Nombre'),
+                      const SizedBox(height: 8), // Espacio entre texto y campo
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          // labelText: 'Nombre',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingresa tu nombre';
+                          }
+                          return null;
+                        },
+                      ),
+                      const Text('Telefono'),
+                      const SizedBox(height: 8), // Espacio entre texto y campo
+                      TextFormField(
+                        controller: _telephoneController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingresa tu telefono';
+                          }
+                          return null;
+                        },
+                      ),
+                      const Text('Departamento'),
+                      const SizedBox(height: 8), // Espacio entre texto y campo
+                      TextFormField(
+                        controller: _stateController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor selecciona un Departamento';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      // Campo de correo
+                      const Text('Email'),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          //labelText: 'Correo Electrónico',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingresa tu email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      // Campo de contraseña
+                      const Text('Contraseña'),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          //labelText: 'Contraseña',
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingresa tu contraseña';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      // Campo de repetir contraseña
+                      const Text('Repetir contraseña'),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        controller: _repitpassController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.transparent),
+                          ),
+                          //labelText: 'Contraseña',
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingresa la contraseña ingresada nuevamente';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 32),
+                      PrimaryButton(
+                        text:
+                            'Registrarse', // Asegúrate de que este texto sea el adecuado
+                        backgroundColor: const Color(0xff1B1C41),
+                        isLoading:
+                            _isLoading, // Aquí puedes manejar el estado de carga
+                        onPressed: _isLoading
+                            ? null
+                            : () {
+                                if (_formKey.currentState!.validate()) {
+                                  _register(); // Lógica de registro
+                                }
+                              },
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+        ));
   }
 
   void _register() {
     // Aquí puedes manejar la lógica de registro
     String name = _nameController.text;
+    String tel = _telephoneController.text;
+    String state = _stateController.text;
     String email = _emailController.text;
     String password = _passwordController.text;
+    String repitPass = _repitpassController.text;
 
     // Realiza validaciones o envía los datos a una API, etc.
     print('Usuario registrado: $name, Correo: $email');
